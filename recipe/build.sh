a011f9bf13850ffecc51d38791579ade96e9b3ac
Wes McKinney <wesm@users.noreply.github.com>
2016-12-20 16:18:48 -0500

    Build both shared and static libs. override lib/ install path (#2)

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,19 @@
 
 set -e
 
+# Build shared libraries
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       -DBUILD_SHARED_LIBS=on .
 
 make -j${CPU_COUNT}
 ctest -V
+make install
+
+# Also build static libs
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+      -DBUILD_SHARED_LIBS=off .
+
+make -j${CPU_COUNT}
 make install
