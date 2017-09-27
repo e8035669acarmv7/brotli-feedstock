d2962c326d6685dd79294a140cbd709ad30b9e03
Wes McKinney <wesm@users.noreply.github.com>
2017-09-27 11:20:17 -0400

    Build with optimizations on Linux / macOS (#7)

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+BROTLI_CFLAGS="-O3"
+BROTLI_CXXFLAGS="-O3"
+
 # Build shared libraries
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+      -DCMAKE_C_FLAGS=$BROTLI_CFLAGS \
+      -DCMAKE_CXX_FLAGS=$BROTLI_CXXFLAGS \
       -DBUILD_SHARED_LIBS=on .
 
 make -j${CPU_COUNT}
@@ -14,6 +19,8 @@ make install
 # Also build static libs
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+      -DCMAKE_C_FLAGS=$BROTLI_CFLAGS \
+      -DCMAKE_CXX_FLAGS=$BROTLI_CXXFLAGS \
       -DBUILD_SHARED_LIBS=off .
 
 make -j${CPU_COUNT}
