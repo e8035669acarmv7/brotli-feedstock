d43966e97666ff909168a466dd0a5396db1c9639
Dougal J. Sutherland <dougal@gmail.com>
2018-02-01 17:59:47 +0000

    bump to 1.0.2 (#9)
    * bump to 1.0.2
    
    also install with cmake on windows
    
    * MNT: Re-rendered with conda-smithy 2.4.5
    
    * windows: tell cmake right place to install
    
    * remove outdated comment [skip ci] [ci skip]
    
    * reduce silliness in windows build
    
    * skip double-build on unix too  [skip appveyor]

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,23 +5,12 @@ set -e
 BROTLI_CFLAGS="-O3"
 BROTLI_CXXFLAGS="-O3"
 
-# Build shared libraries
+# Build both static and shared libraries
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       -DCMAKE_C_FLAGS=$BROTLI_CFLAGS \
-      -DCMAKE_CXX_FLAGS=$BROTLI_CXXFLAGS \
-      -DBUILD_SHARED_LIBS=on .
+      -DCMAKE_CXX_FLAGS=$BROTLI_CXXFLAGS
 
 make -j${CPU_COUNT}
 ctest -V
-make install
-
-# Also build static libs
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
-      -DCMAKE_C_FLAGS=$BROTLI_CFLAGS \
-      -DCMAKE_CXX_FLAGS=$BROTLI_CXXFLAGS \
-      -DBUILD_SHARED_LIBS=off .
-
-make -j${CPU_COUNT}
 make install
