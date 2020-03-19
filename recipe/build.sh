2e769b1cef614b77d5cf4534280b9e9ff8ce76dc
Uwe L. Korn <xhochy@users.noreply.github.com>
2020-03-19 08:05:54 +0100

    Move to Azure for Windows (#20)

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,14 +3,15 @@
 set -e
 
 BROTLI_CFLAGS="-O3"
-BROTLI_CXXFLAGS="-O3"
 
 # Build both static and shared libraries
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       -DCMAKE_C_FLAGS=$BROTLI_CFLAGS \
-      -DCMAKE_CXX_FLAGS=$BROTLI_CXXFLAGS
+      -GNinja \
+      -DCMAKE_BUILD_TYPE=Release \
+      .
 
-make -j${CPU_COUNT}
+ninja
 ctest -V
-make install
+ninja install
